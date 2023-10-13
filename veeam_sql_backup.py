fbd542e3c7670fe815d938cba578e29ecdc43832
Daniel <116485787+dkwagner40@users.noreply.github.com>
2023-10-13 14:34:01 +0200

    Update veeam_sql_backup.py

--- veeam_sql_backup.py
+++ veeam_sql_backup.py
@@ -4,3 +4,7 @@ import json
 veeam_api_url = "add url"
 veaam_api_username = "my_username"
 veamm_api_password = "my_password"
+
+session = requests.Session()
+session.verify = False # If needed for Veeam server SSL certificate
+

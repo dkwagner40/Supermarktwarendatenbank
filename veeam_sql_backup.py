b2746bdcd10dccfb4b17b8903477f996ddcc60ee
Daniel <116485787+dkwagner40@users.noreply.github.com>
2023-10-14 12:23:05 +0200

    Update veeam_sql_backup.py

--- veeam_sql_backup.py
+++ veeam_sql_backup.py
@@ -5,6 +5,9 @@ veeam_api_url = "https://your-veeam-server/api"
 veeam_api_username = "your_username"
 veeam_api_password = "your_password"
 
+]with open("supermarket_goods.json", "r") as json_file:
+    supermarket_goods_data = json.load(json_file)
+
 session = requests.Session()
 session.verify = False  # Might use it if Veeam server has a self-signed SSL certificate
 
@@ -13,7 +16,9 @@ login_payload = {
     "grant_type": "password",
     "username": veeam_api_username,
     "password": veeam_api_password,
+    "supermarket_goods": supermarket_goods  # Add the JSON data to the payload
 }
+
 response = session.post(login_url, json=login_payload)
 token_data = response.json()
 
@@ -28,7 +33,6 @@ if "access_token" in token_data:
     print("List of Veeam jobs:")
     for job in jobs_data:
         print(f"Job ID: {job['Id']}, Name: {job['Name']}")
-
 
 else:
     print("Authentication failed. Check your credentials.")
